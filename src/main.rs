15e105e88f19801e18c0a69aae59e02d31833899
yoshijulas <yoshijulas@live.com>
2024-04-16 23:29:17 -0600

    Refactor main game loop and split functionality into `imprimir_tabla` and `adivinar_tabla` functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::{prelude::SliceRandom, thread_rng, Rng};
+use rand::{prelude::SliceRandom, thread_rng};
 
 fn read_input() -> String {
     let mut input_str = String::new();
@@ -11,52 +11,56 @@ fn read_input() -> String {
 fn main() {
     loop {
         println!("Opcion 1: Imprimir tabla con numero. \nOpcion 2: Adivinar el numero. \nOpcion -1: Salir");
-        let option: isize = read_input().trim().parse().unwrap();
-        if option == -1 {
-            break;
-        }
-
-        println!("Elije el numero de la tabla");
-
-        let num: usize = read_input().trim().parse().unwrap();
-
-        if num > 0 && num < 11 && option == 1 {
-            for (num, it, result) in generar_tabla(num) {
-                println!("\t\t{num} x {it} = {result}");
-            }
-        }
-
-        let mut numeros = [false; 10];
-
-        if num > 0 && num < 11 && option == 2 {
-            let mut numbs = generar_tabla(num);
-            // numbs.shuffle(&mut thread_rng());
-
-            // print!("{numbs:?}");
-            let mut i = 0;
-            while i < 10 {
-                for _ in 0..numbs.len() {
-                    let rand = rand::thread_rng().gen_range(0..10);
-
-                    if !numeros[rand] {
-                        let (num, it, result) = numbs[rand];
-                        println!("{num} x {it} = ");
-                        i += 1;
-                        numeros[rand] = true;
-
-                        for _ in 0..3 {
-                            let user_try: usize = read_input().trim().parse().unwrap();
-
-                            if result == user_try {
-                                println!("Correcto! :)");
-                                break;
-                            }
-
-                            println!("Intenta de nuevo :(");
-                        }
+        let option = read_input().trim().parse::<isize>();
+        match option {
+            Ok(-1) => break,
+            Ok(1 | 2) => loop {
+                println!("Elije el numero para generar la tabla");
+                let num: isize = read_input().trim().parse().unwrap();
+                match (num > 0, num < isize::MAX / 10, option.as_ref().unwrap()) {
+                    (true, true, 1) => imprimir_tabla(num.try_into().unwrap()),
+                    (true, true, 2) => adivinar_tabla(num.try_into().unwrap()),
+                    (false, _, _) => {
+                        println!("Introduce un numero positivo");
+                        continue;
                     }
+                    (_, false, _) => {
+                        println!("El numero es muy grande");
+                        continue;
+                    }
+                    _ => continue,
                 }
+            },
+            _ => {
+                println!("Introduce una opcion valida");
+                continue;
             }
+        }
+    }
+}
+
+fn imprimir_tabla(num: usize) {
+    for (num, it, result) in generar_tabla(num) {
+        println!("\t\t{num} x {it} = {result}");
+    }
+}
+
+fn adivinar_tabla(num: usize) {
+    let mut numbs = generar_tabla(num);
+    numbs.shuffle(&mut thread_rng());
+
+    for (num, it, result) in numbs {
+        println!("{num} x {it} = ");
+
+        for _ in 0..3 {
+            let user_try: usize = read_input().trim().parse().unwrap();
+
+            if result == user_try {
+                println!("Correcto! :)");
+                break;
+            }
+
+            println!("Intenta de nuevo :(");
         }
     }
 }
